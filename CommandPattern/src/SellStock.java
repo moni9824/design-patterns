371093d3d1e300bf3ad2379eb3c5f23b81ab40e9
Simona Vlasova <simona.vlasova@gmail.com>
2020-04-22 14:50:49 +0300

    Commit Project

---
+++ CommandPattern/src/SellStock.java
@@ -0,0 +1,16 @@
+  
+public class SellStock implements Order {
+
+    private Stock abcStock;
+
+    public SellStock(Stock abcStock) {
+        this.abcStock = abcStock;
+    }
+
+    
+
+	public void execute() {
+        abcStock.sell();
+    }
+
+}
